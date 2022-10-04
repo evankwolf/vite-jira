164e50e715fc177ab791c0f2984750ebbac53123
Evan Aki <evanakihito@outlook.com>
2022-10-05 02:00:42 +0800

    feat: 添加try-use-array并封装useArray练手

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProjectList from './screens/project-list'
+// import TryUseArray from './components/try-use-array'
 // import {SearchPanel} from './screens/project-list/search-panel'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      {/* <TryUseArray /> */}
       <ProjectList />
     </div>
   )
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 /**
  * 清除对象中值为undefined/null的键值对
  * @param {*} obj
@@ -14,4 +16,36 @@ function cleanObject(obj: object) {
   return res
 }
 
-export { cleanObject }
+const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+const useArray = <T>(params: T[])  => {
+  const [value, setValue] = useState(params)
+  const clear = () => {
+    setValue([])
+  }
+  const removeIndex = (index: number) => {
+    if (index < 0 ) throw new Error("索引有问题兄弟")
+    const newArr = [...value.slice(0, index), ...value.slice(index + 1)]
+    setValue(newArr)
+  }
+  const add = (param: T) => {
+    const newArr = [...value, param]
+    setValue(newArr)
+  }
+  return {
+    value,
+    clear,
+    removeIndex,
+    add,
+  }
+}
+
+export { 
+  cleanObject,
+  useMount,
+  useArray,
+}
